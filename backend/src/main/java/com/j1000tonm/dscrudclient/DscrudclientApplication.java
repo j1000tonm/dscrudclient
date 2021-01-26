bda1a34a9fd93060f003a1c754c52074457413f2
José Milton <j1000tonm.jmb@gmail.com>
2021-01-26 17:03:19 -0300

    Project created

---
+++ backend/src/main/java/com/j1000tonm/dscrudclient/DscrudclientApplication.java
@@ -0,0 +1,13 @@
+package com.j1000tonm.dscrudclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscrudclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscrudclientApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/j1000tonm/dscrudclient/DscrudclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.j1000tonm.dscrudclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscrudclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
